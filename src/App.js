9b6c5cc77c922de28ca4ef60ed73d846ef93d722
Brionna White <Brionnanwhite@gmail.com>
2016-12-22 20:28:52 -0800

    load data in data.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
+    console.log('our data is', this.props.data)
     return (
         <ul>
-          <li className="boy"> Name 1</li>
-          <li className="girl"> Name 1</li>
-          <li className="boy"> Name 2</li>
-          <li className="girl"> Name 1</li>
-          <li className="boy"> Name 3</li>
-          <li className="girl"> Name 1</li>
+        <li>{this.props.name}</li>
+        <li>{this.props.age}</li>
 
+          <li className="boy">Name 1</li>
+          <li className="girl">Name 1</li>
+          <li className="boy">Name 2</li>
+          <li className="girl">Name 1</li>
+          <li className="boy">Name 3</li>
+          <li className="girl">Name 1</li>
         </ul>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import data from './data/data'; // import data.js
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <App data={data}/>, // passing data from data.js
   document.getElementById('root')
 );
